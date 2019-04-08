9e63a4ab539abf18c76f949dc8b49b1808eadfd8
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-04-09 00:25:19 +0100

    finish bubble_sort_by method

--- Bubble-sort/sorting.rb
+++ Bubble-sort/sorting.rb
@@ -16,3 +16,24 @@ end
 
 #print bubble_sort([1,8,9,7,2,5,0,20,3])
 
+def bubble_sort_by (a)
+    ch=false
+    until ch 
+        ch=true 
+        (a.length-1).times do |i|
+            d=yield(a[i],a[i+1])
+            if d>0
+                tmp=a[i]
+                a[i]=a[i+1]
+                a[i+1]=tmp
+                ch=false
+            end
+        end
+    end 
+    
+    p a
+end
+
+bubble_sort_by(["hi","hello","hey","bycicle","o"]) do |left,right|
+      left.length - right.length
+ end
