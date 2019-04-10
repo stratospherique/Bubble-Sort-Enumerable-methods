118882d4bd577da0851fbc8b168c5717092f28e2
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-04-10 23:12:47 +0100

    create my_all my_any my_non methods

--- Enumerable-Methods/modules.rb
+++ Enumerable-Methods/modules.rb
@@ -50,11 +50,50 @@ module Enumerable
         end
         puts res 
         return res     
-    end 
+    end
+    def my_any(&block)
+        res=false
+        if block_given?
+             my_select do |element| 
+                d= yield element 
+                 if d==true 
+                    res=true 
+                    break
+                 end 
+            end
+        else
+            my_select do |element|
+                if element.nil? || element==false
+                    res=true
+                    break
+                end
+            end
+        end
+        puts res 
+        return res     
+    end
+    
+    def my_none(&block)
+        res=true 
+        if block_given?
+             my_select do |element| 
+                d= yield element 
+                 if d
+                    res=false
+                    break
+                 end 
+            end
+        else
+            res=false
+        end
+        puts res 
+        return res     
+    end
 end
 
 #p [1,2,3,6].my_each {|x| x+=1}
 [1,2,3,6].my_each_with_index {|x,item| p item}
 {"cat"=>0, "dog"=>1, "wombat"=>2}.my_each_with_index{|i,item| p item}
 [1,2,3,6].my_select {|x| x<6}
-[1,5,3].my_all {|x| x<6}
+[1,5,3,9].my_none {|x| x>6}
+p [1,5,3,8].none? {|x| x>6}
