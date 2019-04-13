bed84a4e1b324abc41650ea68f8c547e07ee55c5
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-04-13 14:40:56 +0100

    fixing style using rubocop

--- Enumerable-Methods/modules.rb
+++ Enumerable-Methods/modules.rb
@@ -1,166 +1,179 @@
-#the module below contains self-made Enumerable methods that behaves like the original ones
+# the module below contains self-made Enumerable methods that behaves like the original ones
 module Enumerable
   def my_each(*)
-    tmp=self.to_a
-    for i in (0...self.length)
-      tmp[i].size>0 ? yield(tmp[i][0],tmp[i][1]) :yield(i,tmp[i]) 
+    tmp = self.to_a
+    res=[]
+    for i in (0...tmp.length)
+      tmp[i].is_a?(Array) ? d = yield(tmp[i][0],tmp[i][-1]) : yield(i, tmp[i])
+      res << d if d.is_a?(Hash)
     end
-    return tmp
+    return res if res.size>0
+    tmp
   end
 
   def my_each_with_index(*)
-    i=0
-    each do |element| 
-      yield(element,i) 
-      i+=1  
+    i = 0
+    each do |element|
+      yield(element, i)
+      i += 1
     end
   end
-  #my select
+
+  # my select
   def my_select(*)
-    kr=[]
-    my_each do |element| 
-      d= yield element 
-      kr << element if d 
+    kr = []
+    my_each do |element|
+      d = yield element
+      kr << element if d
     end
-    return kr    
-  end 
-  #my all
+    kr
+  end
+
+  # my all
   def my_all(*)
-    res=true
+    res = true
     if block_given?
-      my_select do |element| 
-        d= yield element 
-          if d==false
-            res=false
-            break
-          end 
+      my_select do |element|
+        d = yield element
+        if d == false
+          res = false
+          break
+        end
       end
     else
       my_select do |element|
-        if element.nil? || element==false
-          res=false
+        if element.nil? || element == false
+          res = false
           break
         end
       end
     end
-     res     
+    res
   end
 
-  #my any?
+  # my any?
   def my_any(*)
-    res=false
+    res = false
     if block_given?
-      my_select do |element| 
-        d= yield element 
-          if d==true 
-            res=true 
-            break
-          end 
+      my_select do |element|
+        d = yield element
+        if d == true
+          res = true
+          break
+        end
       end
     else
       my_select do |element|
-        if element.nil? || element==false
-          res=true
+        if element.nil? || element == false
+          res = true
           break
         end
       end
     end
-    res     
+    res
   end
-  #my none
+
+  # my none
   def my_none(*)
-    res=true 
+    res = true
     if block_given?
-      my_select do |element| 
-        d= yield element 
+      my_select do |element|
+        d = yield element
         if d
-          res=false
+          res = false
           break
-        end 
+        end
       end
     else
-        res=false
+      res = false
     end
-    return res     
+    res
   end
-  #my count
+
+  # my count
   def my_count(*)
-    res=0
+    res = 0
     if block_given?
-      my_each do |element| 
-        d= yield element 
-        res+=1 if d 
-      end
-    else 
       my_each do |element|
-        res+=1  
-      end 
-    end
-    
-    return res
-  end
-  #my map
-  def my_map (a=[])
-    res=[]
-    tmp=self.to_a
-    if block_given?
-      for i in (0...self.length)
-        if (tmp[i]).is_a?(Array)
-          d= yield tmp[i][0],tmp[i][1]
-        else 
-          d=yield tmp[i]  
-        end
-        res << d
+        d = yield element
+        res += 1 if d
       end
     else
-      for i in (0...self.length)
-        if (tmp[i]).is_a?(Array)
-          d= a.call(tmp[i][0],tmp[i][1])
-        else 
-          d=a.call(tmp[i])  
-        end
-        res << d
+      my_each do |_element|
+        res += 1
       end
     end
-    return res if res.size>0  
+
+    res
   end
-#inject method
-def my_inject(arg = nil)
-  arr = self.class == Array ? self.slice(0..-1) : self.class == Range ? self.to_a : self.my_map {|k, v| v }
-  acc = arg == nil ? arr[0] : arg
-  nxt = arg == nil ? arr[1] : arr[0]
-  start = arg == nil ? 1 : 0
-  for i in start...arr.size
-    res = yield(acc,nxt)
-    acc = res
-    nxt = arr[i+1]
+
+  # my map
+  def my_map(a = [])
+    res = []
+    tmp = self.to_a
+    is_array= tmp[0].is_a?(Array) ? true : false 
+    if block_given?
+      if is_array
+        my_each do |key,value|
+        d=yield key, value
+        res << d
+        end
+      else
+        my_each do |item|
+          d=yield item
+          res << d
+        end
+      end 
+    else
+      if is_array
+        my_each do |key,value|
+        d=a.call(key, value)
+        res << d
+        end
+      else
+        my_each do |item|
+          d=a.call(item)
+          res << d
+        end
+      end
+    end
+    return res unless res.empty?
   end
-  acc
-end
-    
+
+  # inject method
+  def my_inject(arg = nil)
+    arr = self.class == Array ? slice(0..-1) : self.class == Range ? to_a : my_map { |_k, v| v }
+    acc = arg.nil? ? arr[0] : arg
+    nxt = arg.nil? ? arr[1] : arr[0]
+    start = arg.nil? ? 1 : 0
+    (start...arr.size).each do |i|
+      res = yield(acc, nxt)
+      acc = res
+      nxt = arr[i + 1]
+    end
+    acc
+  end
 end
 
-#multiply test
+# multiply test
 def mulitply_els(a)
-  a.my_inject(1){|total,item| total*item}
+  a.my_inject(1) { |total, item| total * item }
 end
 
-p [1,2,3,6].my_each {|x| x+1}
-[1,2,3,6].my_each_with_index {|x,item| p item}
-{"cat"=>0, "dog"=>1, "wombat"=>2}.my_each{|i,item| puts "#{i} : #{item}"}
-{"cat"=>0, "dog"=>1, "wombat"=>2}.my_each_with_index{|val,index| puts "#{val} : #{index}"}
-=begin
-[1,2,3,6].my_select {|x| x<6}
-[1,5,3,9].my_none {|x| x>6}
-my_proc=Proc.new do |x| 
-    x+1
-end
-hash={"cat"=>0, "dog"=>1, "wombat"=>2}
-p hash.class 
-p [1,5,3,8,5,7].my_map {|x| x+1}
-p [1,5,3,8,5,7].my_map(my_proc)
-p hash.my_map {|key,value| {key=>value}}
-p [0,5,8,7,5,2].my_inject(0){|total,item| total+item}
-p hash.my_inject{|total,item| total+item}
-p mulitply_els([2,5,8])
-=end
+p [1, 2, 3, 6].my_each { |x| x + 1 }
+[1, 2, 3, 6].my_each_with_index { |_x, item| p item }
+{ 'cat' => 0, 'dog' => 1, 'wombat' => 2 }.my_each { |i, item| puts "#{i} : #{item}" }
+{ 'cat' => 0, 'dog' => 1, 'wombat' => 2 }.my_each_with_index { |val, index| puts "#{val} : #{index}" }
+#p [1,2,3,6].my_select {|x| x<6}
+#p [1,5,3,9].my_none {|x| x>6}
+# my_proc=Proc.new do |x|
+#     x+1
+# end
+ hash={"cat"=>0, "dog"=>1, "wombat"=>2}
+# p hash.class
+#p [1,5,3,8,5,7].my_map {|x| x+1}
+# p [1,5,3,8,5,7].my_map(my_proc)
+ p hash.my_map {|key,value| {key=>value}}
+# p [0,5,8,7,5,2].my_inject(0){|total,item| total+item}
+# p hash.my_inject{|total,item| total+item}
+# p mulitply_els([2,5,8])
